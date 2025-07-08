af411c832547b01e9b499e07d26a6f028cbe2594
sahibakhan0 <zarakhan040905@gmail.com>
2025-07-08 03:16:05 -0700

    arithmetic project committed by sahiba

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+package javaclasses;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+    int a = 10; 
+    int b = 20;
+    int sum = a+b; 
+    int sub = b-a;
+    int mult = a*b;
+    int div = b/a;
+    int rem = a%b;
+    
+    
+    
+    System.out.println(sum);
+    System.out.println(sub);
+    System.out.println(mult);
+    System.out.println(div);
+    System.out.println(rem);
+	}
+
+}
