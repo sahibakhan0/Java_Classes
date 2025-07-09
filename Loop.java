c4d7415d6857e068f02b47d0ec67e25d7ba85fff
sahibakhan0 <zarakhan040905@gmail.com>
2025-07-09 00:08:20 -0700

    loop & pattern committed by sahiba

---
+++ Loop.java
@@ -0,0 +1,30 @@
+package javaclasses;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		
+		
+//for loop
+	 for(int i=0;i<=100;i++) {
+	 System.out.println(i);
+	 }
+	 
+//while loop
+	 int i=0;
+	 while(i<=5) {
+	 System.out.println(i);
+	 i++;
+	 }
+	 
+//do while loop
+	 int j=0;
+	 do{
+		 System.out.println(j);
+	 j++;
+	 }while(j<5); 
+	 
+	}
+
+}
